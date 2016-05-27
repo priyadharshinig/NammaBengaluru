5d4573fea38a1350da60d78c57233ba344653da0
priya.dharshini <priya.dharshini@flipkart.com>
2016-05-27 13:35:04 +0530

    Adding weather info

--- app/src/main/java/com/example/priyadharshini/nammabengaluru/Weather.java
+++ app/src/main/java/com/example/priyadharshini/nammabengaluru/Weather.java
@@ -1,30 +1,52 @@
 package com.example.priyadharshini.nammabengaluru;
 
-import android.location.Location;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.client.HttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class Weather extends AppCompatActivity {
 
-    LocationTracker locationTracker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-
-        locationTracker = new LocationTracker();
-        double latitude = locationTracker.latitude;
-        double longitude = locationTracker.longitude;
-        final TextView textA = (TextView) findViewById(R.id.lat);
-        textA.setText(latitude+"");
-        final TextView text = (TextView) findViewById(R.id.longitude);
-        textA.setText(longitude+"");
-
+        String responseJson = null;
+        try {
+            responseJson = getWeather();
+            JSONObject reader = new JSONObject(responseJson);
+            JSONObject main  = reader.getJSONObject("main");
+//            JSONObject weather  = reader.getJSONObject("weather");
+            String temperature = main.getString("temp");
+//            String icon = weather.getString("icon");
+//            String description = weather.getString("description");
+            TextView menuText = (TextView) findViewById(R.id.weather);
+            String tempString = "Current temperature in Bangalore is: "+ temperature;
+            menuText.setText(tempString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    private String getWeather() throws ExecutionException, InterruptedException {
 
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=bangalore,India&APPID=b3a9591134e378601fb3f14eee925f4a&units=metric";
 
+        AsyncTask<String, String, String> response = new OpenWeatherMapHelper().execute(url);
+        return  response.get();
+
+    }
 
 
 }
